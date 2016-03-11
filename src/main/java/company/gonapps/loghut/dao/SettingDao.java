44641ef2c2893b34ddc4671353d731dc3ebec984
Byeonggon Lee <gonny952@gmail.com>
2016-03-12 06:10:49 +0900

    Removed preferences api

--- src/main/java/company/gonapps/loghut/dao/SettingDao.java
+++ src/main/java/company/gonapps/loghut/dao/SettingDao.java
@@ -2,7 +2,6 @@ package company.gonapps.loghut.dao;
 
 import java.io.IOException;
 import java.util.Properties;
-import java.util.prefs.Preferences;
 
 import org.springframework.stereotype.Repository;
 
@@ -10,15 +9,13 @@ import org.springframework.stereotype.Repository;
 public class SettingDao {
 	
     private Properties properties;
-    private Preferences preferences;
     
     private SettingDao()  throws IOException {
     	properties = new Properties();
         properties.load(this.getClass().getResourceAsStream("/settings.properties"));
-        preferences = Preferences.userRoot().node("loghut");
     }
     
     public String getSetting(String key) {
-		return preferences.get(key, properties.getProperty(key));
+		return properties.getProperty(key);
 	}
 }
